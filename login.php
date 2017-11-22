07efb38b651f9f3daeb139ab76d0e0321bc3b0eb
unknown <alfredo161996@gmail.com>
2017-11-22 11:50:45 +0100

    Renamed login.html in .php and added include of navbar and footer

--- login.php
+++ login.php
@@ -23,47 +23,9 @@
         <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
   </head>
   <body>
-    <div class="jumbotron">
-      <div class="container text-center">
-        <h1>Piotto</h1>
-        <p>Pizza e Panzerotto</p>
-      </div>
-    </div>
-    <nav class="navbar navbar-inverse">
-      <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Piotto</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Chi Siamo</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Prodotti<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Hamburger</a></li>
-            <li><a href="#">Pinse</a></li>
-            <li><a href="#">Panzerotti</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Dicono di noi</a></li>
-        <li><a href="#">Contatti</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Accedi</a></li>
-        <li><a href="#">Carrello</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    <?php
+      include 'navbar.php';
+    ?>
   <!-- Top content -->
         <div class="top-content">
 
@@ -163,7 +125,9 @@
         </div>
 
         <!-- Footer -->
-
+        <?php
+            include 'footer.php';
+        ?>
         <!-- Javascript -->
         <script src="assets/js/jquery-1.11.1.min.js"></script>
         <script src="assets/bootstrap/js/bootstrap.min.js"></script>
